140062a608a00048d65a694a0569f7bf1529ee42
Yan Cheng Cheok <yccheok@yahoo.com>
2015-12-30 23:41:13 +0800

    Fetch map from web.

--- src/skiing/Skiing.java
+++ src/skiing/Skiing.java
@@ -5,9 +5,14 @@
  */
 package skiing;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,6 +26,11 @@ public class Skiing {
     public static void main(String[] args) {
         // TODO code application logic here
         Map map = getMap();
+        
+        if (map == null) {
+            System.out.println("null map");
+            return;
+        }
         
         Skier skier = new Skier(map);
      
@@ -38,11 +48,16 @@ public class Skiing {
                 }
             }            
         }
-        
-        bestPath.print();
+        if (bestPath == null) {
+            System.out.println("null");
+        } else {
+            System.out.println(bestPath.size()+ "" + bestPath.steep()+ "@redmart.com");
+        }
+        //bestPath.print();
     }
     
     public static Map getMap() {
+        /*
         int row = 4;
         int col = 4;
         int[] _elevations = new int[] {
@@ -55,6 +70,29 @@ public class Skiing {
         List<Integer> elevations = new ArrayList<>();
         for (int e : _elevations) {
             elevations.add(e);
+        }
+        */
+        URL url;
+        try {
+            url = new URL("http://s3-ap-southeast-1.amazonaws.com/geeks.redmart.com/coding-problems/map.txt");
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Skiing.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        Scanner s;
+        try {
+            s = new Scanner(url.openStream());
+        } catch (IOException ex) {
+            Logger.getLogger(Skiing.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
+        int row = s.nextInt();
+        int col = s.nextInt();
+        
+        List<Integer> elevations = new ArrayList<>();
+        while (s.hasNext()) {
+            elevations.add(s.nextInt());
         }
         
         return new Map(row, col, elevations);
